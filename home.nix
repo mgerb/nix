90a76a89b6525b0f324e01f53dce1d532322ae67
Mitchell <mgerb42@gmail.com>
2024-07-04 16:50:58 +0000

    use neovim as editor

--- home.nix
+++ home.nix
@@ -68,7 +68,7 @@
   #  /etc/profiles/per-user/nixos/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "neovim";
   };
 
   # Let Home Manager install and manage itself.
