31ec9db1e8d45e829f1894d99c6652ce47f66390
Mitchell <mgerb42@gmail.com>
2024-07-16 23:55:31 -0500

    home-server: update firewall

--- home-server/modules/networking.nix
+++ home-server/modules/networking.nix
@@ -12,16 +12,30 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
       22 # ssh
       2999 # mgdocker
       1883 # mqtt
+
+      # plex
+      1900
+      5353
+      8324
+      32400
+      32410
+      32412
+      32413
+      32414
+      32469
+    ];
+    allowedTCPPortRanges = [
+      {
+        # docker containers
+        from = 5000;
+        to = 5100;
+      }
     ];
   };
 }
